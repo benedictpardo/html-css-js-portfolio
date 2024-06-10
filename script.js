465c79cde3282cb73a2527aaee0cffa686285a1a
benedictpardo <benedictpardo@gmail.com>
2024-06-10 23:39:40 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var currentYear = new Date().getFullYear();
+    document.getElementById('current-year').textContent = currentYear;
+});
